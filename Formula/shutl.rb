b55c255b63d3330f69731dbad7db907378deb661
Korbinian Stoemmer <korbinian.stoemmer@sap.com>
2025-04-28 11:59:06 +0200

    add formula

---
+++ Formula/shutl.rb
@@ -0,0 +1,20 @@
+class Shutl < Formula
+  desc "A command-line tool for organizing, managing, and executing scripts as commands"
+  homepage "https://github.com/k15r/shutl"
+  url "https://github.com/k15r/shutl/archive/refs/tags/v0.0.4.tar.gz"
+  sha256 "52c45aaa7f16e1cb72123bb937c89f2b66616c897cecd727f114eadf47ffd57e"
+  license "MIT"
+  head "https://github.com/k15r/shutl.git", branch: "main"
+
+  depends_on "rust" => :build
+
+  def install
+    system "cargo", "install", *std_cargo_args
+    generate_completions_from_executable(bin/"shutl", shell_parameter_format: :clap)
+
+  end
+
+  test do
+    assert_match "A command-line tool for organizing, managing, and executing scripts as commands", shell_output("#{bin}/shutl --help | head -n 1")
+  end
+end
---
+++ README.md
@@ -0,0 +1,18 @@
+# K15r Shutl
+
+## How do I install these formulae?
+
+`brew install k15r/shutl/<formula>`
+
+Or `brew tap k15r/shutl` and then `brew install <formula>`.
+
+Or, in a `brew bundle` `Brewfile`:
+
+```ruby
+tap "k15r/shutl"
+brew "<formula>"
+```
+
+## Documentation
+
+`brew help`, `man brew` or check [Homebrew's documentation](https://docs.brew.sh).
