9db7424bf547ff0dc13d9aafb64643088fefd591
Korbinian Stoemmer <korbinian.stoemmer@sap.com>
2025-04-28 15:28:27 +0200

    lint-fix

--- Formula/shutl.rb
+++ Formula/shutl.rb
@@ -1,5 +1,5 @@
 class Shutl < Formula
-  desc "command-line tool for organizing, managing, and executing scripts as commands"
+  desc "CLI tool for organizing, managing, and executing scripts as commands"
   homepage "https://github.com/k15r/shutl"
   url "https://github.com/k15r/shutl/archive/refs/tags/v0.0.4.tar.gz"
   sha256 "52c45aaa7f16e1cb72123bb937c89f2b66616c897cecd727f114eadf47ffd57e"
@@ -15,7 +15,7 @@ class Shutl < Formula
   end
 
   test do
-    assert_match "A command-line tool for organizing, managing, and executing scripts as commands", 
+    assert_match "A command-line tool for organizing, managing, and executing scripts as commands",
       shell_output("#{bin}/shutl --help | head -n 1")
   end
 end
